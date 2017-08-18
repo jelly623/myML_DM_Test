f1297f9fddf1a1d2f01c2a4c30acd2d751d8a3d3
daijitao <daijitao@ctsi.com.cn>
2017-08-18 17:19:22 +0800

    add ml

--- src/ml/dataMinding.py
+++ src/ml/dataMinding.py
@@ -2,3 +2,10 @@
 
 # page 22 python数据挖掘入门与实践
 # http://archive.ics.uci.edu/ml/datasets/Ionosphere
+# print os.path.expanduser("~") 用户主目录
+import numpy as np
+import csv
+
+data_filename = "C:\\Users\\daijitao\\data\\ionosphere.data"
+X = np.zeros((351,34),dtype='float')
+y = np.zeros((351,), dtype='bool') #1行
